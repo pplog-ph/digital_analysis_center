ca9cc8d90882382a5a8cebdf01c403582d43237d
pplog-ph <85467278+pplog-ph@users.noreply.github.com>
2023-07-20 11:05:54 +0900

    visible view add

--- pybo/views/update_views.py
+++ pybo/views/update_views.py
@@ -7,7 +7,7 @@ from ..models import Question
 def report_update(request):
     page = request.GET.get('page', '1')   # 페이지
     question_list = Question.objects.annotate(subscribe_count=Count('subscribe')).order_by('-subscribe_count')
-    paginator = Paginator(question_list, 10)  # 페이지 당 10개씩 보여주기
+    paginator = Paginator(question_list, 20)  # 페이지 당 10개씩 보여주기
     page_obj = paginator.get_page(page)
     context = {'question_list': page_obj}
     return render(request, 'pybo/report_update.html', context)
